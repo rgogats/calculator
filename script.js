94e35e1bef23250c8642ab80e61cf85653e3ccb0
Ryan Gogats <ryangogats@ryans-mbp.mynetworksettings.com>
2023-11-09 10:45:32 -0500

    fix display bugs, round decimal answers, handle long expressions

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const displayContainer = document.querySelector('.display-container');
-// const aDisplay = document.querySelector('.a-display');
-// const bDisplay = document.querySelector('.b-display');
-// const operatorDisplay = document.querySelector('.operator-display');
+// let displayText = displayContainer.innerText;
 const digitButtons = document.querySelectorAll('.digit');
 const decimalButton = document.querySelector('#decimal');
 const equalsButton = document.querySelector('button#equals');
@@ -11,9 +9,8 @@ const clearButton = document.querySelector('#clear');
 const calculator = () => {
     // fn operate which takes 2 operands and 1 operator
     // has to handle addition, subtraction, multiplication, division
-    let a = '';
-    let b = '';
-    let operator = '';
+    // ***could separate display refresh function and underlying calculations***
+    let [a, b, operator] = ['', '', ''];
 
     console.log('operator buttons', operatorButtons);
     console.log('digit buttons', digitButtons);
@@ -21,21 +18,36 @@ const calculator = () => {
     // display user input
     const setOperator = (e) => {
         console.log('Operator selected', e.target.innerText);
-        displayContainer.innerText = e.target.innerText;
         operator = e.target.innerText;
-        console.log('a=', a, 'operator=', operator, 'b=', b);
-        return e.operator;
+        a && b ? (
+            // operator = e.target.innerText,
+            a = calculateResult(),
+            b = '',
+            console.log('a=', a, 'operator=', operator, 'b=', b)
+        )
+        : (
+            // operator = e.target.innerText,
+            console.log('a=', a, 'operator=', operator, 'b=', b)
+        )
+        // handle display refresh if operator already added
+        console.log('updated operator display', displayContainer.innerText.replace(/[+\-x÷]/, e.target.innerText));
+        displayContainer.innerText.match(/[+\-x÷]/) ? displayContainer.innerText = displayContainer.innerText.replace(/[+\-x÷]/, e.target.innerText)
+        : displayContainer.innerText += operator;
+        
+        return operator;
     } 
 
     const setOperand = (e) => {
         // needs conditional logic for Operator.  Operator Set ? => add operand B : add to operand A
         a && operator ? (
             console.log('OPERAND B', e.target.innerText),
+            // displayText.replace('^0+(?!$)', ''),
             displayContainer.innerText += e.target.innerText,
             b += e.target.innerText,
             console.log('a=', a, 'operator=', operator, 'b=', b)
         ) : (
             console.log('OPERAND A', e.target.innerText),
+            // displayText.replace('^0+(?!$)', ''),
             displayContainer.innerText += e.target.innerText,
             a += e.target.innerText,
             console.log('a=', a, 'operator=', operator, 'b=', b)
@@ -44,21 +56,30 @@ const calculator = () => {
 
     const operate = () => {
         // all 3 values must be truthy, not null
-        console.log('CALCULATING A:', a, ' ', operator, 'B: ', b);
+        console.log('CALCULATING A:', a, 'operator:', operator, 'B: ', b);
         a = parseFloat(a);
         b = parseFloat(b);
-        return (a && b && operator &&
+        return (
+        //if only operand A, return A
+        !b && a ? a 
+        : (
+        //operate full expression
+        a && b && operator &&
         operator == '+' ? a + b
-        : operator == '-' ? a + b
+        : operator == '-' ? a - b
         : operator == 'x' ? a * b
-        : operator == '÷' ? a / b
-        : 'Something went wrong');
+        : operator == '÷' ? b === 0 ? 'YASSIFICATION ACTIVATED' : a / b
+        : 'Something went wrong'
+        ))
     };
 
-    const displayResult = () => {
-        const result = operate(a, operator, b);
-        console.log(a, operator, b, '=', result);
+    const calculateResult = () => {
+        const result = operate(a, operator, b).toFixed(2);
         displayContainer.innerText = result;
+        console.log('result', result);
+        [a, operator, b] = [result, '', ''];
+        console.log('post-calculation:', 'A', a, 'operator', operator, 'B', b);
+        return result;
     };
 
     const clearCalculator = () => {
@@ -79,7 +100,7 @@ const calculator = () => {
         button.addEventListener('click', setOperator);
     });
     decimalButton.addEventListener('click', setOperand);
-    equalsButton.addEventListener('click', displayResult);
+    equalsButton.addEventListener('click', calculateResult);
     clearButton.addEventListener('click', clearCalculator);
 };
 
