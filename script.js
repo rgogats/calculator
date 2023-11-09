5204ac9c68eb7a74256c6f38c7f7400a89a6593d
Ryan Gogats <rgogats1@gmail.com>
2023-11-08 19:17:14 -0500

    implement basic display and operation logic

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
+const aDisplay = document.querySelector('.a-display');
+const bDisplay = document.querySelector('.b-display');
+const operationDisplay = document.querySelector('.operation-display');
+const buttons = document.querySelectorAll('button');
+const equalsButton = document.querySelector('button#equals');
+
 const calculator = () => {
     // fn operate which takes 2 operands and 1 operator
     // has to handle addition, subtraction, multiplication, division
-    const operate = (operandA, operator, operandB) => {
-        return operator == '+' ? operandA + operandB
-        : operator == '-' ? operandA + operandB
-        : operator == '*' ? operandA * operandB
-        : operator == '/' ? operandA / operandB
+    let a;
+    let b;
+    let operator;
+    
+    const operate = () => {
+        // all 3 values must be truthy, not null
+        return a && b && c &&
+        operator == '+' ? a + b
+        : operator == '-' ? a + b
+        : operator == '*' ? a * b
+        : operator == '/' ? a / b
         : 'Something went wrong';
     };
 
-    // fn to handle calculator display
-    // should store display
+    // display user input
+    const selectOperation = (e) => {
+        console.log('set operation to', e.target.innerText);
+        operatorDisplay.innerText = e.target.innerText;
+        operator = e.target.innerText;
+        return e.operation;
+    } 
 
-    // fn clear to clear function? 
+    const selectDigit = (e) => {
+        console.log('selected digit', e.target.innerText);
+        // needs conditional logic for operation.  Operation Set ? => add operand B : add to operand A
+        aDisplay.innerText += e.target.innerText;
+    };
+    [...buttons].forEach(() => {
+        this.innerText != "=" || this.innerText != "Clear" ? this.addEventListener('click', selectDigit) : this.addEventListener('click', selectOperation);
+    });
+
+    console.log(equalsButton);
+    equalsButton.addEventListener('click', operate);
+
+    // store display variable
 };
 
 calculator();
